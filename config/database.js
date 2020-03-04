baf8a9481d3cfba5be4698ba7ab266a2deee3dbf
salih saadi <salih-sadi@hotmail.com>
2020-03-04 12:04:23 +0200

    asaaaaa

---
+++ config/database.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+ //mongoDbUrl : 'mongodb://localhost:27017/cms'
+
+  mongoDbUrl : 'mongodb+srv://saleh:Saleh1994@cms-lyp1d.mongodb.net/test?retryWrites=true&w=majority'
+
+
+};
