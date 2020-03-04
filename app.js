a10cfdfef31c61b7720536a12f45b6c62da65799
salih saadi <salih-sadi@hotmail.com>
2020-03-04 11:21:32 +0200

    DASDAS

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -12,55 +11,64 @@ const flash = require('connect-flash');
 const {mongoDbUrl} = require('./config/database');
 const passport = require('passport');
 
+
 mongoose.Promise = global.Promise;
 
-mongoose.connect(mongoDbUrl,()=>{
+
+mongoose.connect(mongoDbUrl).then(db =>{
+
+    console.log('MONGO connected');
+
+}).catch(error=> console.log(error));
 
 
-	console.log('connected');
-});
 
-
-
+// Using Static
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-// set view Engine 
-
-const {select, generateTime, paginate} = require('./helpers/handlebars-helpers');
-
-app.engine('handlebars', exphbs({defaultLayout:'home', helpers: {select:select, generateTime:generateTime, paginate:paginate}}));
-app.set('view engine','handlebars');
-
-//method override
-app.use(methodOverride('_method'));
+// Set View Engine
 
 
-//uploade middlware
+const {select, generateDate, paginate} = require('./helpers/handlebars-helpers');
+app.engine('handlebars', exphbs({defaultLayout: 'home', helpers: {select: select, generateDate: generateDate, paginate: paginate}}));
+app.set('view engine', 'handlebars');
+
+// Upload Middleware
+
+
 app.use(upload());
 
-//body parser
-app.use(bodyParser.urlencoded({extended:true}));
+// Body Parser
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+// Method Override
+
+app.use(methodOverride('_method'));
+
 app.use(session({
- 
- secret: 'saleh',
- resave: true,
- saveUninitialized:true
+
+    secret: 'edwindiaz123ilovecoding',
+    resave: true,
+    saveUninitialized: true
 
 }));
-
 app.use(flash());
 
-//passport
+// PASSPORT
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-//local varibales using middleware
-app.use((req, res, next)=>{
 
+
+
+// Local Variables using Middleware
+
+
+app.use((req, res, next)=>{
 
     res.locals.user = req.user || null;
 
@@ -76,7 +84,10 @@ app.use((req, res, next)=>{
 
 
 });
-//load routes
+
+
+
+// Load Routes
 
 const home = require('./routes/home/index');
 const admin = require('./routes/admin/index');
@@ -84,7 +95,7 @@ const posts = require('./routes/admin/posts');
 const categories = require('./routes/admin/categories');
 const comments = require('./routes/admin/comments');
 
-//use routes
+// Use Routes
 
 app.use('/', home);
 app.use('/admin', admin);
@@ -92,10 +103,11 @@ app.use('/admin/posts', posts);
 app.use('/admin/categories', categories);
 app.use('/admin/comments', comments);
 
+
 const port = process.env.PORT || 4500;
 
 app.listen(port, ()=>{
 
-console.log('app is run');
+console.log(`listening on port 4500`);
 
 });
